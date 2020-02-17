ce92db9568633cb1d1ce0a65163f7f476f57e74b
yujiwinet <60724081+yujiwinet@users.noreply.github.com>
2020-02-17 13:32:54 -0600

    Reversing coordinates of round 0
    Reversing coordinates of xstart and ystart for round 0.

--- mirror_trace_YW.js
+++ mirror_trace_YW.js
@@ -18,10 +18,10 @@
 var materials = {
 		'mirror' : [true, true, true, true, true, true, true, true],
 		'file_names' : ["https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/sample.png", "https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/trialh1.png", "https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/trialh2.png", "https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/trialh3.png", "https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/trial1.png", "https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/trial2.png", "https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/trial3.png", "https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/sample.png"],
-		'xstarts' : [47,	27,		40,		280,		27, 40, 280, 33],
-		'ystarts' : [256,	275,	45,		276,		275, 45, 276, 250],
-		'xends' :   [344,	370,	368,	33,			370, 368, 33, 47],
-		'yends' :    [260,	28,		267,	250,		28,267,250, 256]
+		'xstarts' : [256,	27,		40,		280,		27, 40, 280, 33],
+		'ystarts' : [47,	275,	45,		276,		275, 45, 276, 250],
+		'xends' :   [260,	370,	368,	33,			370, 368, 33, 47],
+		'yends' :    [344,	28,		267,	250,		28,267,250, 256]
 	}
 	
 	//	'xstarts' : [47,	27,		40,		280,		40,		383,	352],
